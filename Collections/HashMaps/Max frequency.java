5922c62e801c402066a5b41a144b826c58e04eaf
Archana-0205-07 <archana020507@gmail.com>
2026-05-06 17:52:29 +0530

    Implement Max Frequency Finder in Java
    This program reads an array of integers and calculates the maximum frequency of any element, printing both the maximum frequency and the corresponding element.

---
+++ Collections/HashMaps/Max frequency.java
@@ -0,0 +1,46 @@
+import java.util.*;
+public class Maxfreq{
+public static void main(String[] args){
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+int[] arr=new int[n];
+for(int i=0;i<n;i++){
+arr[i]=sc.nextInt();
+}
+
+HashMap<Integer,Integer> freq=new HashMap<>();
+
+int maxfreq=0;
+int maxele=0;
+
+
+for(int i=0;i<n;i++){
+
+if(freq.containsKey(arr[i])){
+int current=freq.get(arr[i]);
+freq.put(arr[i],current+1);
+}
+else{
+freq.put(arr[i],1);
+}
+}
+
+for(int i=0;i<n;i++){
+if(freq.containsKey(arr[i])){
+int current=freq.get(arr[i]);
+if(current>maxfreq){
+
+maxfreq=current;
+maxele=arr[i];
+
+}
+}
+}
+System.out.println(maxfreq);
+
+
+System.out.println(maxele);
+
+}
+}
+
