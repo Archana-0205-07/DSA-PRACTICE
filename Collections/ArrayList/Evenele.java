f5fe729a84a3c42011c96aacefd8781c22d1fe46
Archana-0205-07 <archana020507@gmail.com>
2026-05-22 22:23:30 +0530

    Add Evenele class to print even numbers from a list

---
+++ Collections/ArrayList/Evenele.java
@@ -0,0 +1,15 @@
+import java.util.ArrayList;
+public class Evenele{
+public static void main(String[] args){
+ArrayList<Integer>list =new ArrayList<>();
+list.add(20);
+list.add(30);
+list.add(10);
+for(int i=0;i<list.size();i++){
+if(list.get(i)%2==0){
+System.out.println(list.get(i));
+}
+}
+
+}
+}
